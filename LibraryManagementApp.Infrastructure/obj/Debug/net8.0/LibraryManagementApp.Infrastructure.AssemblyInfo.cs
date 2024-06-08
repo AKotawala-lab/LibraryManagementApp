a81950d1e5049a5003909b9e122141e3ec4cfcb1
Adnan Kotawala <adnan.kotawala@gmail.com>
2024-06-08 12:33:11 +0530

    testing commit

--- LibraryManagementApp.Infrastructure/obj/Debug/net8.0/LibraryManagementApp.Infrastructure.AssemblyInfo.cs
+++ LibraryManagementApp.Infrastructure/obj/Debug/net8.0/LibraryManagementApp.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93033179b65d33521300032da2a6824e2c9507f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- LibraryManagementApp.Tests/obj/Debug/net8.0/LibraryManagementApp.Tests.AssemblyInfo.cs
+++ LibraryManagementApp.Tests/obj/Debug/net8.0/LibraryManagementApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93033179b65d33521300032da2a6824e2c9507f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
